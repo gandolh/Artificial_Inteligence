4bb8e30382c544b564f7ceb5e1142045d8e2c837
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-02-06 22:52:31 +0200

    minor fixes to find a spot for a house, it's getting harder those times

--- self_driving_cars/js/world.js
+++ self_driving_cars/js/world.js
@@ -143,9 +143,11 @@ class World {
             bases.push(new Envelope(seg, this.buildingWidth).poly);
         }
 
+        const eps = 0.001;
         for (let i = 0; i < bases.length - 1; i++)
             for (let j = i + 1; j < bases.length; j++)
-                if (bases[i].intersectsPoly(bases[j])) {
+                if (bases[i].intersectsPoly(bases[j]) || 
+                    bases[i].distanceToPoly(bases[j]) < this.spacing - eps){
                     bases.splice(j, 1);
                     j--;
                 }
