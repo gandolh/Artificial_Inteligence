309c38b8d0dd06d3402c011f349ac044b5c6eb16
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-02-08 12:09:31 +0200

    added trafic

--- self-driving-car/js/car.js
+++ self-driving-car/js/car.js
@@ -1,5 +1,5 @@
 class Car {
-    constructor(x, y, width, height) {
+    constructor(x, y, width, height, controlType, maxSpeed = 3) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -7,29 +7,36 @@ class Car {
 
         this.speed = 0;
         this.acceleration = 0.2;
-        this.maxSpeed = 5;
+        this.maxSpeed = maxSpeed;
         this.friction = 0.05;
         this.angle = 0;
         this.damaged = false;
 
-        this.sensor = new Sensor(this);
-        this.controls = new Controls();
+        if(controlType != "DUMMY")
+            this.sensor = new Sensor(this);
+        this.controls = new Controls(controlType);
     }
 
-    update(roadBorders) {
+    update(roadBorders, traffic) {
         if(!this.damaged){
             this.#move();
             this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBorders);
+            this.damaged = this.#assessDamage(roadBorders, traffic);
         }
-        this.sensor.update(roadBorders);
+
+        if(this.sensor)
+            this.sensor.update(roadBorders, traffic);
     }
 
-    #assessDamage(roadBorders) {
+    #assessDamage(roadBorders, traffic) {
         for (let i = 0; i < roadBorders.length; i++) {
-            if (polysIntersect(this.polygon, roadBorders[i])) {
+            if (polysIntersect(this.polygon, roadBorders[i]))
                 return true;
-            }
+        }
+
+        for (let i = 0; i < traffic.length; i++) {
+            if (polysIntersect(this.polygon, traffic[i].polygon))
+                return true;
         }
         return false;
     }
@@ -92,16 +99,17 @@ class Car {
         return points;
     }
 
-    draw(ctx) {
+    draw(ctx, {color = 'black'}= {}) {
         if (this.damaged)
             ctx.fillStyle = 'gray';
-        else ctx.fillStyle = 'black';
+        else ctx.fillStyle = color;
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
         for (let i = 1; i < this.polygon.length; i++)
             ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
         ctx.fill();
-        this.sensor.draw(ctx);
+        if(this.sensor)
+            this.sensor.draw(ctx);
     }
 
     moveUp() {
--- self-driving-car/main.js
+++ self-driving-car/main.js
@@ -4,18 +4,28 @@ canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1),100, 30, 50);
+const car = new Car(road.getLaneCenter(1),100, 30, 50, "KEYS");
+const traffic = [
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY",2),
+]
 
 animate();
 
 function animate() {
-    car.update(road.borders);
+    for(const t of traffic) {
+        t.update(road.borders,[]);
+    }
+    car.update(road.borders, traffic);
     canvas.height = window.innerHeight;
 
     ctx.save();
     ctx.translate(0, -car.y + canvas.height * 0.7);
     road.draw(ctx);
-    car.draw(ctx);
+    for(let i=0; i< traffic.length;i++){
+        traffic[i].draw(ctx,{color: 'red'});
+    }
+
+    car.draw(ctx, {color: 'blue'});
 
     ctx.restore();
     requestAnimationFrame(animate);
